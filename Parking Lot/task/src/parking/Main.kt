74a0a9a88ef15fa78c7e3b0d753f4fe9642bfdd5
alicenychto <irol2273@icloud.com>
2020-06-16 21:00:51 +0200

    stage 2

--- Parking Lot/task/src/parking/Main.kt
+++ Parking Lot/task/src/parking/Main.kt
@@ -1,7 +1,41 @@
 package parking
 
+data class Car(val number: String, val color: String)
+
 fun main() {
-    println("White car has parked.")
-    println("Yellow car left the parking lot.")
-    println("Green car just parked here.")
+    val parkingLot = Array<Car?>(2) { null }
+    parkingLot[0] = Car("test", "test")
+    while (true) {
+        val line = readLine() ?: break
+        val cmd = line.split(" ")
+        when (cmd[0]) {
+            "park" -> {
+                when {
+                    parkingLot[0] == null -> {
+                        val car = Car(cmd[1], cmd[2])
+                        parkingLot[0] = car
+                        println("${car.color} car parked in spot 1.")
+                    }
+                    parkingLot[1] == null -> {
+                        val car = Car(cmd[1], cmd[2])
+                        parkingLot[1] = car
+                        println("${car.color} car parked in spot 2.")
+                    }
+                    else -> {
+                        println("There is no space available.")
+                    }
+                }
+            }
+            "leave" -> {
+                val num = cmd[1].toInt() - 1
+                if (parkingLot[num] == null)
+                    println("There is no car in spot ${num+1}.")
+                else {
+                    parkingLot[num] = null
+                    println("Spot ${num+1} is free.")
+                }
+            }
+            else -> {}
+        }
+    }
 }
