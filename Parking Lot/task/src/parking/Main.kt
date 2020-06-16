70bf9754fe16ce0a0d8662daac077b203f1b3569
alicenychto <irol2273@icloud.com>
2020-06-16 21:29:01 +0200

    stage 3

--- Parking Lot/task/src/parking/Main.kt
+++ Parking Lot/task/src/parking/Main.kt
@@ -2,40 +2,46 @@ package parking
 
 data class Car(val number: String, val color: String)
 
+class ParkingLot(size: Int) {
+    private val spaces: Array<Car?> = Array(20) { null }
+
+    private fun nextAvailable(): Int = spaces.indexOf(null)
+
+    fun park(num: String, color: String) {
+        when (val i = nextAvailable()) {
+            -1 -> println("Sorry, the parking lot is full.")
+            else -> {
+                val car = Car(num, color)
+                spaces[i] = car
+                println("${car.color} car parked in spot ${i+1}.")
+            }
+        }
+    }
+    fun leave(num: Int) {
+        if (spaces[num - 1] == null)
+            println("There is no car in spot $num.")
+        else {
+            spaces[num - 1] = null
+            println("Spot $num is free.")
+        }
+    }
+}
+
 fun main() {
-    val parkingLot = Array<Car?>(2) { null }
-    parkingLot[0] = Car("test", "test")
+    val parkingLot = ParkingLot(20)
     while (true) {
-        val line = readLine() ?: break
+        val line = readLine()!!
+        if (line == "exit")
+            break
         val cmd = line.split(" ")
         when (cmd[0]) {
             "park" -> {
-                when {
-                    parkingLot[0] == null -> {
-                        val car = Car(cmd[1], cmd[2])
-                        parkingLot[0] = car
-                        println("${car.color} car parked in spot 1.")
-                    }
-                    parkingLot[1] == null -> {
-                        val car = Car(cmd[1], cmd[2])
-                        parkingLot[1] = car
-                        println("${car.color} car parked in spot 2.")
-                    }
-                    else -> {
-                        println("There is no space available.")
-                    }
-                }
+                parkingLot.park(cmd[1], cmd[2])
             }
             "leave" -> {
-                val num = cmd[1].toInt() - 1
-                if (parkingLot[num] == null)
-                    println("There is no car in spot ${num+1}.")
-                else {
-                    parkingLot[num] = null
-                    println("Spot ${num+1} is free.")
-                }
+                parkingLot.leave(cmd[1].toInt())
             }
-            else -> {}
+            else -> throw IllegalArgumentException()
         }
     }
 }
